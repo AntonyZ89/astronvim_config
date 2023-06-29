1fdafbca56110aa4c61144cb3803aa11366d88e8
Antony <antonyz.dev@gmail.com>
2023-06-29 03:59:28 -0300

    fix: lint and formatter from local

--- plugins/community.lua
+++ plugins/community.lua
@@ -13,17 +13,4 @@ return {
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.typescript" },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    opts = {
-      handlers = {
-        prettierd = function()
-          local null_ls = require "null-ls"
-          -- disable prettierd
-          -- FIX: Not working. Override prettierd and disable it
-          null_ls.register(null_ls.builtins.formatting.prettierd.with { condition = function() return false end })
-        end
-      }
-    }
-  },
 }
